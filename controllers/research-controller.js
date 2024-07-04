3c31c97725ed601c5ededbe38f68bbaef266ba55
Sujith Varughese <sujithv15@gmail.com>
2024-07-04 16:14:40 -0400

    functions for get listing details - logic working

--- controllers/research-controller.js
+++ controllers/research-controller.js
@@ -1,18 +1,9 @@
 import { StatusCodes } from 'http-status-codes'
 import axios from 'axios'
 
-const connectRealtyAPI = axios.create({
-  baseURL: "https://realty-in-us.p.rapidapi.com/properties/v3",
-  headers: {
-    'Content-Type': 'application/json',
-    'x-rapidapi-host': process.env['X-RAPIDAPI-HOST'],
-    'x-rapidapi-key': process.env['X-RAPIDAPI-KEY']
-  }
-})
-
 const baseUrl = "https://realty-in-us.p.rapidapi.com/properties/v3"
 const listingsUrl = "/list"
-const listingDetailsUrl = "/detail"
+const listingDetailsUrl = "/detail?property_id="
 const getListingsZipCode = async (req, res) => {
   const { zipCode } = req.body
   console.log(zipCode)
@@ -57,25 +48,31 @@ const getListingsZipCode = async (req, res) => {
 
 const getListingDetails = async (req, res) => {
   const { id } = req.params
-
-  const response = await connectRealtyAPI(`/${id}`)
-
-
-  const { home: results } = response.data.data
-
-  const home = results.map(result => {
-    return {
-      propertyId: result.property_id,
-      description: result.description.text,
-      yearBuilt: result.description.year_built,
-      photos: result.photos.map(photo => photo.href),
-      listDate: result.list_date,
-      lastSoldPrice: result.last_sold_price,
-      lastSoldDate: result.last_sold_date,
-      link: result.href,
+  const url = `${baseUrl}${listingDetailsUrl}${id}`
+  console.log(url)
+  const response = await axios.get(url, {
+    headers: {
+      'Content-Type': 'application/json',
+      'x-rapidapi-host': process.env['X-RAPIDAPI-HOST'],
+      'x-rapidapi-key': process.env['X-RAPIDAPI-KEY']
     }
   })
 
+  const { home: result } = response.data.data
+
+  const home = {
+    propertyId: result.property_id,
+    description: result.description.text,
+    yearBuilt: result.description.year_built,
+    photos: result.photos.map(photo => photo.href),
+    listDate: result.list_date,
+    lastSoldPrice: result.last_sold_price,
+    lastSoldDate: result.last_sold_date,
+    link: result.href,
+  }
+
+
+  console.log(home)
   res.status(StatusCodes.OK).json({ home })
 
 }
