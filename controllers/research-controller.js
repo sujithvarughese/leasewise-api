11a9c537f0963a26e33b51ee9b3be81ad1b3c3ae
sujithvarughese <sujithv15@gmail.com>
2024-09-15 14:06:53 -0400

    updated links

--- controllers/research-controller.js
+++ controllers/research-controller.js
@@ -5,9 +5,6 @@ const baseUrl = "https://realty-in-us.p.rapidapi.com/properties/v3"
 const listingsUrl = "/list"
 const listingDetailsUrl = "/detail?property_id="
 
-const host = process.env['X-RAPIDAPI-HOST']
-const key = process.env['X-RAPIDAPI-KEY']
-
 const getListingsZipCode = async (req, res) => {
   const { zipCode } = req.body
   console.log(zipCode)
@@ -23,8 +20,8 @@ const getListingsZipCode = async (req, res) => {
   const response = await axios.post(`${baseUrl}${listingsUrl}`, JSON.stringify(requestConfig), {
     headers: {
       'Content-Type': 'application/json',
-      'x-rapidapi-host': "realty-in-us.p.rapidapi.com",
-      'x-rapidapi-key': "0435c9e626msh69ef4c61d151b29p143368jsn3b1769d8ff96"
+      'x-rapidapi-host': process.env['X_RAPIDAPI_HOST'],
+      'x-rapidapi-key': process.env['X_RAPIDAPI_KEY']
     }
   })
   const { home_search: data } = response.data.data
@@ -60,8 +57,8 @@ const getListingDetails = async (req, res) => {
   const response = await axios.get(url, {
     headers: {
       'Content-Type': 'application/json',
-      'x-rapidapi-host': "realty-in-us.p.rapidapi.com",
-      'x-rapidapi-key': "0435c9e626msh69ef4c61d151b29p143368jsn3b1769d8ff96"
+      'x-rapidapi-host': process.env['X_RAPIDAPI_HOST'],
+      'x-rapidapi-key': process.env['X_RAPIDAPI_KEY']
     }
   })
 
